7ddbfcee6b52b740adbac2b20901b4ee06fa2395
Nick Burgess <nick@teguchi.org>
2012-11-22 15:07:22 -0600

    added some more basice. Build server list from group files etc...

--- nsh.rb
+++ nsh.rb
@@ -2,6 +2,7 @@
 # TODO
 # exclude servers from group lists
 # allow domain suffix
+# allow for username per host
 
 
 class Nsh
@@ -12,11 +13,12 @@ class Nsh
 
 	# Set some defaults
 
-	def initialize (targets, type)
+	def initialize (targets, type, user)
 		@targets = Array.new
 		@targets |= targets
 		@group_path = "~/.nsh/groups/"
-		set_type(type)
+		@user = user
+		@type = set_type(type)
 
 		if @type =~ /^gs?$/
 			build_server_list
@@ -28,7 +30,7 @@ class Nsh
 			print "Error! Type must be \"g\", \"gs\" or \"s\".\n"
 			return 1
 		else
-			@type = type
+			type
 		end
 	end
 
@@ -38,18 +40,40 @@ class Nsh
 			regroup = File.expand_path(@group_path + group)
 			@server_list |= File.readlines(regroup)
 			@server_list.sort!
-			@server_list.
+			@server_list.uniq!
 		end
+		@server_list.each_with_index {|line, index| @server_list[index]=line.chomp!}
 		p @server_list
 	end
 
-	def parse_flags ()
+	def exclude_servers (exclude)
+		exclude.each {|item| @server_list.delete(item)}
 	end
 
-	def run_command (command)
+	def run_command (commands)
+		@output = Array.new
+		Net::SSH.start('host', 'user', :password => 'password') do |ssh|
+			commands.each do |command|
+				ssh.exec!(command)
+			end
+		end
 	end
 
 end
 
-conn = Nsh.new(['ksplit', 'ksplit_dev'], 'g')
+def parse_flags ()
+	options = {}
+	optparse = OptionParser.new do |opts|
+		opts.banner = "Usage: #{$0} [options] domain /doc/root/path"
+		options[:drupal] = false
+		opts.on('-d', '--drupal', 'Add options for drupal') do 
+			options[:drupal] = true
+		end
+	end
+	optparse.parse!
+end
+
+if __FILE__ == $0
+	conn = Nsh.new(['ksplit', 'ksplit_dev'], 'g')
+end
 
