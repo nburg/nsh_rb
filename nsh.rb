024d51ee73f26f6ea0d57447a803e1784b5fabc0
Nick <ficus@jin>
2012-12-11 19:34:01 -0600

    Changed options to a hash, removed ostruct. Initialize now just takes a hash arguement for handy optional defaults.

--- nsh.rb
+++ nsh.rb
@@ -1,49 +1,55 @@
 #!/usr/bin/ruby
 # TODO
-# exclude servers from group lists
 # allow domain suffix
 # allow for username per host
-# groups of groups
 # make host sorting an option
 # add configuration file
-
+# confirmation flag
 
 class Nsh
   require 'optparse'
   require 'rubygems'
   require 'net/ssh'
-  require 'ostruct'
 
-  attr_accessor :password, :group_path, :host_list, :host, :ssh_user, :options
+  attr_accessor :host_list, :options
   
 
-  def initialize (options)
-    @options    = options
-    build_host_list
+  def initialize (options = {})
+    @options   = {
+      :banner     => true,
+      :commands   => [],
+      :excludes   => [],
+      :group_path => File.expand_path('~/.nsh/groups') + '/',
+      :groups     => [],
+      :hosts      => [],
+      :script     => '',
+      :wait       => 0
+    }.merge(options)
+    @host_list = []
   end
 
-  # Adds to @host_list from groups specified in @options.groups
+  # Adds to @host_list from groups specified in @options[:groups]
   def add_groups(group)
-    File.readlines(@options.group_path + group).each {|line| @host_list << line.chomp}
+    File.readlines(@options[:group_path] + group).each {|line| @host_list << line.chomp}
   end
 
-  # Adds groups of groups to @host_list
+  # Adds groups of groups to @host_list. Need to add a check for loops.
   def add_group_of_groups (group)
-    File.readlines(@options.group_path + group).each do |line|
-      add_groups(line)
+    File.readlines(@options[:group_path] + group).each do |line|
+      add_groups(line.chomp)
     end
   end
 
   # Adds a list of individual host to @host_list
   def add_hosts
-    @host_list |= @options.hosts
+    @host_list |= @options[:hosts]
   end
 
   # This will give us the variable @host_list. These are the hosts that we want
   # to run commmands on
   def build_host_list
     @host_list = []
-    @options.groups.each do |group|
+    @options[:groups].each do |group|
       # Group files ending in .g will be processed as a groups of groups
       if group =~ /\.g$/
         add_group_of_groups(group)
@@ -52,6 +58,7 @@ class Nsh
       end
     end
     add_hosts
+    exclude_hosts if options[:excludes] != nil
     clean_host_list
     p @host_list
   end
@@ -63,34 +70,30 @@ class Nsh
   end
 
   # Remove 
-  def exclude_servers (exclude)
+  def exclude_hosts (exclude = options[:excludes])
     exclude.each {|item| @host_list.delete(item)}
   end
 
-  def traverse_servers
-  end
-
-  def run_commands (commands = @options.commands)
+  def run_commands (commands = @options[:commands], 
+                    wait = @options[:wait], 
+                    ssh_user = @options[:ssh_user], 
+                    ssh_password = @options[:password])
     @output = Array.new
+    first_run = true
     @host_list.each do |server|
+      sleep wait unless first_run
       puts "---=== #{server} ===---"
-      Net::SSH.start(server, @user, :password => password) do |ssh|
+      Net::SSH.start(server, ssh_user, :password => ssh_password) do |ssh|
         commands.each { |command| ssh.exec(command) }
       end
+      first_run = false
     end
   end
 
-  def self.parse_flags
-    options = OpenStruct.new
+  def run_script
+  end
 
-    options.banner     = true
-    options.commands   = []
-    options.exclude    = []
-    options.group_path = File.expand_path('~/.nsh/groups') + '/'
-    options.groups     = []
-    options.hosts      = []
-    options.script     = ''
-    options.wait       = 0
+  def parse_flags
 
     opts = OptionParser.new do |opts|
       opts.banner = "Usage: #{$0} blah blah"
@@ -103,48 +106,64 @@ class Nsh
       end
 
       opts.on('-c', '--command COMMAND', 'Select groups seperated by commas') do |command|
-        options.commands << command
+        @options[:commands] << command
       end
 
       opts.on('-g', '--groups x,y,z', Array, 'Select groups seperated by commas') do |groups|
-        options.groups = groups
+        @options[:groups] = groups
       end
 
       opts.on('-H', '--hosts x,y,z', Array, 'List of individual hosts to iterate') do |hosts|
-        options.hosts = hosts
+        @options[:hosts] = hosts
       end
 
       opts.on('-l', '--list GROUP', "List hosts in GROUP") do |group|
-        options.list = group
+        @options[:list] = group
       end
 
       opts.on('-p', '--group-path PATH', "Set path to group files") do |path|
-        options.group_path = path
+        @options[:group_path] = path
       end
 
       opts.on('-s', '--script SCRIPT', 'Execute local script on remote hosts') do |script|
-        options.exclude = script
+        @options[:script] = script
       end
 
       opts.on('-w', '--wait SEC', 'Time to wait between executing on hosts') do |sec|
-        options.wait = seconds
+        @options[:wait] = sec.to_i
       end
 
-      opts.on('-x', '--exclude x,y,z', 'Exclude specific hosts from listed groups') do |hosts|
-        options.exclude = hosts
+      opts.on('-x', '--exclude x,y,z', Array, 'Exclude specific hosts from listed groups') do |hosts|
+        @options[:excludes] = hosts
       end
 
     end
     opts.parse!
 
-    options
+    @options
   end
+end
 
+def get_memory_usage
+    `ps -o rss= -p #{Process.pid}`.to_i
+end
+
+def nothing
+  nsh = Nsh.new(:groups     => ['ksplit'], 
+                :commands   => ['uptime && whoami'],
+                :group_path => File.expand_path('~/.nsh/groups') + '/',
+                :exclude    => ['wallaby']
+               )
 end
 
 if __FILE__ == $0
-  nsh = Nsh.new(Nsh.parse_flags)
-  nsh.ssh_user = 't-9nburg'
+# p options[:groups]
+  print get_memory_usage
+  nsh = Nsh.new
+  nsh.parse_flags
+  nsh.build_host_list
   nsh.run_commands
+  print get_memory_usage
 end
+
 
